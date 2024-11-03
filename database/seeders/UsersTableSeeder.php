1e3c15d4c349c7bee74a64b352596fadb35d719a
ahmadzytoon <ahmadzytoon@outlook.com>
2024-11-03 19:16:04 +0300

    3-11

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,32 +19,32 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Ahmad.Orange',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('orange123'),
+            'password' => Hash::make('orange@123'),
             'image' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
         ]);
 
         DB::table('users')->insert([
             'name' => 'Ammar Mesmar',
             'email' => 'ammar.mismar@orange.com',
-            'password' => Hash::make('orange123'),
+            'password' => Hash::make('oarnge@123'),
             'image' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
         ]);
         DB::table('users')->insert([
             'name' => 'Omar Al Mashini',
             'email' => 'omar.almashini@orange.com',
-            'password' => Hash::make('orange123'),
+            'password' => Hash::make('orange@123'),
             'image' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
         ]);
         DB::table('users')->insert([
             'name' => 'Muhannad Abu Maizer',
             'email' => 'muhannad.abumaizar@orange.com',
-            'password' => Hash::make('orange123'),
+            'password' => Hash::make('orange@123'),
             'image' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
         ]);
         DB::table('users')->insert([
             'name' => 'Salah Eddin Mahasneh',
             'email' => 'salaheddin.mahasneh@orange.com',
-            'password' => Hash::make('orange123'),
+            'password' => Hash::make('orange@123'),
             'image' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
         ]);
 
